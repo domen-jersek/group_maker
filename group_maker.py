3ea92237c9bcb23f5eb6373bcee5f23414d80876
domen-jersek <56697594+domen-jersek@users.noreply.github.com>
2022-08-11 15:24:18 +0200

    sortiranje po starosti

--- group_maker.py
+++ group_maker.py
@@ -1,2 +1,6 @@
 import pandas as pd
 
+df = pd.read_csv("https://bit.ly/3kGDuKx", na_values="?")
+
+df['starostne_kategorije'] = pd.cut(df["age"], bins=3, labels=("mladi", "srednjih_let", "stari"))
+print(df.groupby(["starostne_kategorije"])["survived"].sum())
